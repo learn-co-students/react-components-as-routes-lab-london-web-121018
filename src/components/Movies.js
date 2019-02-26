ffa38ed67919fc17ecbcd2612200861d607f1d5e
github username <github email address>
2019-02-26 11:00:06 +0000

    learn submit

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import { movies } from '../data';
+import React from 'react'
+import { movies } from '../data'
 
 const Movies = () => {
+  const allMovies = movies.map(movie => {
+    return (
+      <div key={movie.title}>
+        <p>{movie.title}</p>
+        <p>{movie.time}</p>
+        <ul>
+          {movie.genres.map(genre => (
+            <li>{genre}</li>
+          ))}
+        </ul>
+      </div>
+    )
+  })
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {allMovies}
     </div>
-  );
-};
+  )
+}
 
-export default Movies;
+export default Movies
