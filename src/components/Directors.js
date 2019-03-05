57dbf4db856f936e09e51a65c02ffd60f997fee0
github username <github email address>
2019-03-05 18:28:55 +0000

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -3,10 +3,23 @@ import { directors } from '../data';
 
 const Directors = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className= 'directorlist'>
+    <h1>Directors Page</h1>
+    {directors.map((director, idx)=>{
+        return( 
+        <div key={idx}>
+        <h2>Name: {director.name}</h2>
+        <p>Movies:</p>
+        {director.movies.map((movie, idx)=>{return <li key={idx}>{movie}</li>})}
+        </div>)
+      })}
     </div>
   );
 }
 
 export default Directors
+
+// {
+//   name: 'Scott Derrickson',
+//   movies: ['Doctor Strange', 'Sinister', 'The Exorcism of Emily Rose']
+// }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -3,10 +3,23 @@ import { movies } from '../data';
 
 const Movies = () => {
   return (
-    <div>
-        {/*{code here}*/}
+    <div className= 'movielist'>
+    <h1>Movies Page</h1>
+        {movies.map((movie, idx)=>{
+          return(
+          <div key={idx}>
+          <h2>Name: {movie.title}</h2>
+          <p>Time: {movie.time}</p>
+          <p>Genres:</p>
+          {movie.genres.map((genre, idx)=>{return <li key={idx}>{genre}</li>})}
+          </div>)
+        })}
     </div>
   );
 };
 
 export default Movies;
+
+// title: 'Trolls',
+//     time: 92,
+//     genres: ['Animation', 'Adventure', 'Comedy', 'Family', 'Fantasy']
